746b68975a715edc48c50361bd78713492b52cba
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2022-01-29 23:00:48 -0500

    able to delete old posts in dashboard

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -40,7 +40,26 @@ router.get('/:id',(req,res)=>{
     Posts.findOne({
         where:{
             id: req.params.id
-        }
+        },
+        include:[
+            {
+                model:Users,
+                attributes:['username'],
+                foreignKey:'id'
+            },
+            {
+                model:Comments,
+                foreignKey:'user_id',
+                attributes:['comment_text'],
+                include:[
+                    {
+                        model:Users,
+                        foreignKey:'id',
+                        attributes:['username']
+                    }
+                ]
+            }
+        ]
     })
     .then(db=>res.json(db))
     .catch(er=>{
@@ -71,21 +90,52 @@ router.post('/',(req,res)=>{
 //delete a post by id
 router.get("/del/:id",(req,res)=>{
     console.log(req.params.id)
-    Posts.destroy({
+    //try to get all the comments and then delete them and then 
+    //delete the post...
+    Comments.findAll({
         where:{
-            id:req.params.id
+            post_id:req.params.id
         }
+    }).then(db=>{
+        const comments = db.map(r=>r.get({plain:true}))
+        const commentIds = comments.map(r=>r.id)
+        console.log(commentIds)
+        Comments.destroy({
+            where:{
+                id:commentIds
+            }
+        }).then(d=>{
+            Posts.destroy({
+                where:{
+                    id:req.params.id
+                }
+            })
+            .then(dbPostData => {
+                if (!dbPostData) {
+                  res.status(404).json({ message: 'No post found with this id' });
+                  return;
+                }
+                res.render("dashboard")
+              })
+              .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+              });
+        })
+        .catch(er=>{
+            console.log(er);
+            //res.status(500).json(er)
+        })
     })
-    .then(dbPostData => {
-        if (!dbPostData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
-        res.render("dashboard")
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+    .catch(er=>{
+        console.log(er);
+        //res.status(500).json(er)
+    })
+
+
+
+
+
+    
 })
 module.exports = router;
